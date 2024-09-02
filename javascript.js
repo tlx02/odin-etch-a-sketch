83a3375dd42adf0b6e1755b425c48139709c2a45
tlx02 <e0774379@u.nus.edu>
2024-09-02 20:41:32 +0800

    Add feature to reset the grid, allowing user to decide its size

--- javascript.js
+++ javascript.js
@@ -1,16 +1,28 @@
-container = document.querySelector('#container');
-console.log(container);
+const container = document.querySelector('#container');
+const btn = document.querySelector('button');
 
-size = 16;
+function generateGrid(size) {
+    container.textContent = '';
 
-for (let i=0; i<size; i++) {
-    const row = document.createElement('div');
-    container.appendChild(row);
-    for (let j=0; j<size; j++) {
-        const grid = document.createElement('div');
-        row.appendChild(grid);
-        grid.addEventListener('mouseenter', () => {
-            grid.style.backgroundColor = "Blue";
-        })
+    for (let i=0; i<size; i++) {
+        const row = document.createElement('div');
+        container.appendChild(row);
+        for (let j=0; j<size; j++) {
+            const grid = document.createElement('div');
+            row.appendChild(grid);
+            grid.addEventListener('mouseenter', () => {
+                grid.style.backgroundColor = "Blue";
+            })
+        }
     }
 }
+
+generateGrid(16);
+
+btn.addEventListener('click', () => {
+    let numOfSquares = Number(prompt('How many squares would you like on each side?'));
+    while (numOfSquares > 100) {
+        numOfSquares = Number(prompt('Please input a value not more than 100:'));
+    }
+    generateGrid(numOfSquares);
+});
